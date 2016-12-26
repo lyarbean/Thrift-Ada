406cfd57ea520ac2559a54e7b5492c5af7fe5d0b
yanliebin <yanliebin@kingsoft.com>
2016-12-26 16:13:34 +0800

    Reformat

--- thrift.ads
+++ thrift.ads
@@ -1,27 +1,27 @@
 package thrift is
-   type i08 is mod 2**8;
-   type i16 is mod 2**16;
-   type i32 is mod 2**32;
-   type i64 is mod 2**64;
-   type sizetype is mod 2**32;
-   type messagedatatype is
-      (t_stop,
-       t_void,
-       t_bool,
-       t_byte,
-       t_i08,
-       t_i16,
-       t_i32,
-       t_u64,
-       t_i64,
-       t_double,
-       t_string,
-       t_utf7,
-       t_struct,
-       t_map,
-       t_set,
-       t_list,
-       t_utf8,
-       t_utf16);
-   type messagekind is (t_none, t_call, t_retry, t_exception, t_oneway);
+   type I08 is mod 2**8;
+   type I16 is mod 2**16;
+   type I32 is mod 2**32;
+   type I64 is mod 2**64;
+   type SizeType is mod 2**32;
+   type MessageDataType is
+      (T_Stop,
+       T_Void,
+       T_Bool,
+       T_Byte,
+       T_I08,
+       T_I16,
+       T_I32,
+       T_U64,
+       T_I64,
+       T_Double,
+       T_String,
+       T_Utf7,
+       T_Struct,
+       T_Map,
+       T_Set,
+       T_List,
+       T_Utf8,
+       T_Utf16);
+   type MessageKind is (T_None, T_Call, T_Retry, T_Exception, T_Oneway);
 end thrift;
